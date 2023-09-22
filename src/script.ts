676ac927a9489505ae07638fa2fe271585cae867
AlesiaTelpuk <em000323@g.bstu.by>
2023-09-22 11:18:13 +0300

    feat: create Component Создание базового компонента

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,18 @@
+import { Component } from './Abstact/Component';
 import './style.scss';
+
+const body = document.body;
+
+const btn1 = new Component(body, 'input', ["btnspace"], null, ["type", "value"], ["button", "Отобразить"]);
+
+const btn2 = new Component(body, 'input', ["btnspace"], null, ["type", "value"], ["button", "Удалить"]);
+
+const prg = new Component(body, 'p', null, 'I prefer you');
+
+btn2.root.onclick = () => {
+  prg.remove();
+}
+
+btn1.root.onclick = () => {
+  prg.render();
+}
